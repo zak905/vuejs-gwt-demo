9a0bc6c3c6b00674be8b3002e0158eef99046080
adrienbaron <adrien@abaron.net>
2018-03-18 18:40:55 +0100

    Make DTO objects non JsInterop
    Regular POJO can work in Vue GWT templates, only the collections they
    contains must have the @JsProperty annotation for observation.

--- src/main/java/com/gwidgets/client/dto/Expense.java
+++ src/main/java/com/gwidgets/client/dto/Expense.java
@@ -1,43 +1,26 @@
 package com.gwidgets.client.dto;
 
-
-import com.google.gwt.core.client.GWT;
-import jsinterop.annotations.JsConstructor;
-import jsinterop.annotations.JsProperty;
-import jsinterop.annotations.JsType;
-
-@JsType
 public class Expense {
-
-    @JsConstructor
     public Expense(double amount, String date, String reason, double amountVat, String vatRate, String currency) {
         this.amount = amount;
         this.date = date;
         this.reason = reason;
-        GWT.log("inside constructor" + amountVat);
         this.amountVAT = amountVat;
         this.vatRate = vatRate;
         this.currency = currency;
     }
 
-    @JsProperty
-    public double amount = 0;
+    public double amount;
 
-    @JsProperty
     public String date;
 
-    @JsProperty
     public String reason;
 
-    @JsProperty
-    public double amountVAT = 0;
+    public double amountVAT;
 
-    @JsProperty
     public String vatRate;
 
-    @JsProperty
     public String vat;
 
-    @JsProperty
     public String currency;
 }
